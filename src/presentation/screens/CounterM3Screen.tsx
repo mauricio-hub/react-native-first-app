028aa393237f16f6f5f0c4cb9a6a11d6b9b381d4
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-03-01 07:12:08 -0500

    agregando icons

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {Text, View} from 'react-native';
 import {FAB} from 'react-native-paper';
 import {globalStyles} from '../theme/global.styles';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 export const CounterM3Screen = () => {
   const [counter, setCounter] = useState(10);
 
@@ -10,11 +10,15 @@ export const CounterM3Screen = () => {
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{counter}</Text>
 
+      {/* <Icon name="accessibility-outline" size={25}/> */}
+
       <FAB
-        label='+1'
+       // label=''
         style={globalStyles.fab}
         onPress={() => setCounter(counter + 1)}
         onLongPress={() => setCounter(0)}
+        icon="add-outline"
+       // icon={() => <Icon name="add" size={25} />}
       />
     </View>
   );
